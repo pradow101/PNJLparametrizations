7989f3b3a504fe56f9ed250c0bad7fc1893fe710
joao.prado <joao.prado>
2025-08-04 11:09:29 -0300

    initial commit

---
+++ SRC/parameters.jl
@@ -0,0 +1,21 @@
+GeV = 1
+MeV = 1e-3
+
+Nc = 3
+Nf = 2
+m = 5.6MeV
+L = 0.65
+G = 10.1
+
+a0 = 3.51
+a1 = -2.47
+a2 = 15.2
+b3 = -1.75
+
+To = 270MeV
+
+A0 = a0   ##esses valores para mudar depois, este é um teste.
+A1 = a1
+A2 = a2
+A3 = -0.1
+
