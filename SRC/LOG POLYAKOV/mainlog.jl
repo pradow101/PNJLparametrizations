44667a793fa95901bae180b96a6f6c97f1b3f455
João A. Prado <joao.prado@acad.ufsm.br>
2025-08-20 15:32:05 -0300

    saving

--- SRC/LOG POLYAKOV/mainlog.jl
+++ SRC/LOG POLYAKOV/mainlog.jl
@@ -2,7 +2,7 @@ begin
     include("parameterslog.jl")
     include("functionslog.jl")
 
-    using QuadGK, Plots, NLsolve, CSV, DataFrames, ForwardDiff, DataInterpolations, LocalFunctionApproximation, Interpolations, NonlinearSolve
+    using QuadGK, Plots, NLsolve, CSV, DataFrames, ForwardDiff, DataInterpolations, LocalFunctionApproximation, Interpolations, NonlinearSolve, NPZ
     plotly()
 end
 
@@ -13,9 +13,9 @@ function gapsystem!(du, u, p)
 
     u[1] = clamp(u[1], 0.0, 1.0)
     u[2] = clamp(u[2], 0.0, 1.0)
-    u[3] = max(u[3], 1e-5)
+    u[3] = clamp(u[3], 0.0, 1.2)
 
-    println("Guess: phi = ", round(u[1], digits=3), ", phib = ", round(u[2], digits=3), ", M = ", round(u[3], digits=3), " for μ = ", round(p[1], digits=3), ", T = ", round(p[2], digits=3))
+    println("phi = ", u[1], " phib = ", u[2], " M = ", u[3], " mu = ", p[1], " T = ", p[2])
 
     du[1] = dphilog(u[1],u[2],p[1],p[2],u[3])
     du[2] = dphiblog(u[1],u[2],p[1],p[2],u[3])
@@ -23,30 +23,25 @@ function gapsystem!(du, u, p)
 end
 
 function gapsolver(mu, T)
-    chutealto = [0.01,0.01,0.34]
+    chutealto = [0.01,0.01,0.4]
     chutebaixo = [0.9,0.9,0.01]
     ad = AutoFiniteDiff()
 
     probalto = NonlinearProblem(gapsystem!,chutealto, [mu, T])
-    solalto = solve(probalto, TrustRegion(; autodiff=ad), abstol=1e-8, maxiters = 100)
+    solalto = solve(probalto, TrustRegion(; autodiff=ad), abstol=1e-8)
     probbaixo = NonlinearProblem(gapsystem!,chutebaixo, [mu, T])
-    solbaixo = solve(probbaixo, TrustRegion(; autodiff=ad), abstol=1e-8, maxiters = 100)
+    solbaixo = solve(probbaixo, TrustRegion(; autodiff=ad), abstol=1e-8)
 
-    if abs(solalto.u[3] - solbaixo.u[3]) < 1e-3
-        return [solalto.u[1], solalto.u[2], solalto.u[3], solbaixo.u[1], solbaixo.u[2], solbaixo.u[3]]
+    if abs(solalto.u[3] - solbaixo.u[3]) < 1e-4
+        return [solalto.u[1], solalto.u[2], solalto.u[3]]
     elseif potentiallog(solbaixo.u[1], solbaixo.u[2], mu, T, solbaixo.u[3]) < potentiallog(solalto.u[1], solalto.u[2], mu, T, solalto.u[3])
-        return [solbaixo.u[1], solbaixo.u[2], solbaixo.u[3], solalto.u[1], solalto.u[2], solalto.u[3]]
+        return [solbaixo.u[1], solbaixo.u[2], solbaixo.u[3]]
     end
-    return [solalto.u[1], solalto.u[2], solalto.u[3], solbaixo.u[1], solbaixo.u[2], solbaixo.u[3]]
-end
- 
-
-begin
-    gapsolver(0.2, 0.1)
+    return [solalto.u[1], solalto.u[2], solalto.u[3]]
 end
 
 function solvermurange(mu, T)
-    sols = zeros(length(mu), 6)
+    sols = zeros(length(mu), 3)
     Threads.@threads for i in eachindex(mu)
         sols[i, :] = gapsolver(mu[i], T)
     end
@@ -54,7 +49,7 @@ function solvermurange(mu, T)
 end
 
 function solverTrange(mu, T)
-    sols = zeros(length(T), 6)
+    sols = zeros(length(T), 3)
     Threads.@threads for i in eachindex(T)
         sols[i, :] = gapsolver(mu, T[i])
     end
@@ -62,13 +57,12 @@ function solverTrange(mu, T)
 end
 
 function Tmusolver(mur, Tr)
-    sols = zeros(length(mur), 6, length(Tr))
+    sols = zeros(length(mur), length(Tr), 3)
     Threads.@threads for i in eachindex(Tr)
-        sols[:, :, i] = solvermurange(mur, Tr[i])
+        sols[:, i, :] = solvermurange(mur, Tr[i])
     end
     return sols
 end
-
 
 @time begin
     mur = range(0, 0.6, length = 100)
@@ -77,20 +71,15 @@ end
     scatter(mur, solarr[:,3])
 end
 
-
-
 @time begin
     mur = range(0,0.6,length = 1000)
     Tr = range(0.09, 0.30, length = 30)
     allsols = Tmusolver(mur, Tr)
 end
 
-
 begin
-    scatter(mur, [allsols[:, 3, 1], allsols[:, 3, 25], allsols[:, 3, 30]], xlabel = "μ [GeV]", ylabel = "M [GeV]")
+    plot(mur, [allsols[:, 3, 13], allsols[:, 3, 14], allsols[:, 3, 15]], xlabel = "μ [GeV]", ylabel = "M [GeV]")
 end
-
-
 #Agora para a fase quarkyonica#
 
 function quarkyonic(mu, Msols)
@@ -116,7 +105,83 @@ function quarkyonicall(mur, Tr, sols)
     return mu_quark, T_quark
 end
 
-begin
+let 
+    mur = range(0,0.6,length = 1000)
     muquark, Tquark = quarkyonicall(mur, Tr, allsols)
-    scatter(muquark, Tquark)
+    scatter(muquark, Tquark, xlabel = "μ [GeV]", ylabel = "T [GeV]")
+end
+
+function maxfind(x,y)
+    for i in 100:length(y)-1
+        if y[i+1] < y[i] && y[i-1] < y[i]  
+            return x[i], y[i]
+        end
+    end
+    return NaN, NaN # Return NaN if no maximum is found
+end
+
+begin
+    rightsols = zeros(length(mur), length(Tr), 3)
+    for i in eachindex(Tr)
+        for j in eachindex(mur)
+            rightsols[j,i,1] = allsols[j,1,i]
+            rightsols[j,i,2] = allsols[j,2,i]
+            rightsols[j,i,3] = allsols[j,3,i]
+        end
+    end
+end
+
+
+
+npzwrite("SOLUTIONS.npz", rightsols)
+sols = npzread("SOLUTIONS.npz")
+
+let
+    mur = range(0,0.6,length = 1000)
+    plot(mur, sols[:,15,3], xlabel = "μ [GeV]", ylabel = "M [GeV]")  
+end
+
+
+#=
+Esse right sols é uma matriz 1000x30x3.
+rightsols[:, i, 1] são as soluções de ϕ para o i-ésimo T
+rightsols[:, i, 2] são as soluções de ϕ̄* para o i-ésimo T
+rightsols[:, i, 3] são as soluções de M para o i-ésimo T
+
+Só para não me merder. μ não está guardado em nenhum lugar das soluções.
+=#
+function interp(phi, phib, M, mu)
+    itpM = interpolate((mu,), M, Gridded(Linear()))
+    itpphi = interpolate((mu,), phi, Gridded(Linear()))
+    itpphib = interpolate((mu,), phib, Gridded(Linear()))
+    interp = zeros(length(mu), 3)
+    derinterp = zeros(length(mu), 3)
+    for i in eachindex(mu)
+        interp[i, 1] = itpphi(mu[i])
+        interp[i, 2] = itpphib(mu[i])
+        interp[i, 3] = itpM(mu[i])
+        derinterp[i, 1] = only(Interpolations.gradient(itpphi, mu[i]))
+        derinterp[i, 2] = only(Interpolations.gradient(itpphib, mu[i]))
+        derinterp[i, 3] = -only(Interpolations.gradient(itpM, mu[i]))
+    end
+    return derinterp
+end
+
+function interploop(sols, Tr)
+    phi_int = zeros(length(sols[:,1,1]), length(Tr))
+    phib_int = zeros(length(sols[:,1,1]), length(Tr))
+    M_int = zeros(length(sols[:,1,1]), length(Tr))
+    for i in eachindex(Tr)
+        phi, phib, M, mu = sols[:,1,i], sols[:,2,i], sols[:,3,i], mu[i]
+        interploop = interp(phi, phib, M, T)
+        phi_int[:,i] = interploop[:,1]
+        phib_int[:,i] = interploop[:,2]
+        M_int[:,i] = interploop[:,3]
+    end
+    return phi_int, phib_int, M_int
+end
+
+begin
+    Trange = Tr
+    phi_int, phib_int, M_int = interploop(allsols, Trange)
 end
